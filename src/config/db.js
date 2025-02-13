d6eb7e07541b37767532567699719ff59c3e4ded
panjc <panjc_job@163.com>
2025-02-14 07:55:32 +0800

    整理环境变量

--- src/config/db.js
+++ src/config/db.js
@@ -3,6 +3,7 @@ require('./env')();
 
 const connectDB = async () => {
   try {
+    console.log(`Connecting to MongoDB: ${process.env.MONGODB_URI}`);
     const dbUri = process.env.MONGODB_URI;
     await mongoose.connect(dbUri);
     console.log("MongoDB Connected");
