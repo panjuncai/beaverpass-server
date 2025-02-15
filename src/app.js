d78f8b3aa942c10a6f3cd1cc77ed534e2b2c3364
panjc <panjc_job@163.com>
2025-02-15 20:55:46 +0800

    调整 cors

--- src/app.js
+++ src/app.js
@@ -1,56 +1,68 @@
-require('./config/env')()
-const express=require('express')
-const bodyParser=require('body-parser')
-const cors=require('cors')
-const connectDB=require('./config/db')
-const userRoutes=require('./routes/userRoutes')
-const categoryRoutes=require('./routes/categoryRoutes')
-const productRoutes=require('./routes/productRoutes')
+require("./config/env")();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
 
-const session=require('express-session')
-const {RedisStore}=require('connect-redis')
-const redis=require('redis')
-const redisClient=redis.createClient()
-redisClient.on('error',(err)=>console.error(`Redis error:${err}`))
-redisClient.on('connect',()=>console.log('Connected to Redis'))
-redisClient.connect()// v4 redis should connect
+const session = require("express-session");
+const { RedisStore } = require("connect-redis");
+const redis = require("redis");
+const redisClient = redis.createClient();
+redisClient.on("error", (err) => console.error(`Redis error:${err}`));
+redisClient.on("connect", () => console.log("Connected to Redis"));
+redisClient.connect(); // v4 redis should connect
 
-const app=express()
-const PORT=process.env.PORT||4001
+const app = express();
+const PORT = process.env.PORT || 4001;
 
-connectDB()
+connectDB();
 
-app.use(cors())
-app.use(bodyParser.json())
-
-app.use(session({
-    store:new RedisStore({
-        client:redisClient
-    }),
-    secret:process.env.SECRET_KEY_JWT,
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
-        httpOnly:true,// prevent client access cookie
-        // secure:process.env.NODE_ENV==='production',//必须开启 https才能设置
-        maxAge:600000 // one hour expire
+const allowedOrigins = ['https://www.bigclouder.com', 'https://bigclouder.com','http://localhost:5173'];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, origin);
+    } else {
+      callback(new Error('Not allowed by CORS'));
     }
-}))
+  },
+  credentials: true,
+}));
 
-app.use((req,res,next)=>{
-    console.log(`Request received:${req.method} ${req.url}`)
-    next()
-})
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: process.env.SECRET_KEY_JWT,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true, // prevent client access cookie
+      // secure:process.env.NODE_ENV==='production',//必须开启 https才能设置
+      maxAge: 600000, // one hour expire
+    },
+  })
+);
+
+app.use((req, res, next) => {
+  console.log(`Request received:${req.method} ${req.url}`);
+  next();
+});
 
 // app.get('/',(req,res) => {
 //     res.send("hello");
 // });
 
-app.use('/users',userRoutes)
-app.use('/categories',categoryRoutes)
-app.use('/products',productRoutes)
+app.use("/users", userRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/products", productRoutes);
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
