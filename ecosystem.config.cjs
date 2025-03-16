28273f38f990fa529bc1c8961210ce04bc2bccaf
panjc <panjc_job@163.com>
2025-03-16 14:47:23 +0800

    修复环境变量问题

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -7,6 +7,8 @@ module.exports = {
       node_args: "-r dotenv/config",
       env: {
         NODE_ENV: "production",
+        SUPABASE_URL:"https://xruwlkmlmswfbwfffqkj.supabase.co",
+        SUPABASE_SERVICE_KEY:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhydXdsa21sbXN3ZmJ3ZmZmcWtqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDExODA5ODAsImV4cCI6MjA1Njc1Njk4MH0.SZaR3YVqLLERDIXFoM9uCC6YCKTVlm06FIGyp4NPIW8"
       },
       env_development: {
         NODE_ENV: "development",
